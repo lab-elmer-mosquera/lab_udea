cd5d73d52f35db7e851561a4ac49095d324c224a
Elmer Mosquera <elmer.mosquera@udea.edu.co>
2026-08-29 16:41:07 -0500

    (stwich) version 0.2 *grarantia de entrada fuerte

--- software/practica_1/main.cpp
+++ software/practica_1/main.cpp
@@ -4,35 +4,19 @@
 
 int main() {
     using std::cout, std::cin, std::endl;
-    bool entradaValida{};
-    char entrada[10]{};
     int opcion{};
 
     cout << "Que programa desea ejecutar 1 - " << TOTAL_PROBLEMAS << ": ";
-    cin >> entrada;
-    for (char c : entrada) {
-        if (c >= '0' && c <= '9'){
-            opcion = opcion * 10 + (c - '0');
-            continue;
-        }
-        if (c != 0) {
-            cout << "entrada invalida" << endl;
-            opcion = 0;
-            return 0;
-        }
-    }
+    cin >> opcion;
 
     if (opcion >= 1 && opcion <= 16){
-        entradaValida = true;
+        problemas[opcion-1]();
+        return 0;
     } else {
-        cout << "fuera de rango" << endl;
+        cout << "entrada invalida" << endl;
         return 0;
     }
 
-    if (entradaValida) {
-        problemas[opcion-1]();
-        return 0;
-    }
 
     cout << "error desconocido" << endl;
     return 1;
