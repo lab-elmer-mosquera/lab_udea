bf0154338b975858d1fc82660fea382d7086b2ba
Elmer Mosquera <elmer.mosquera@udea.edu.co>
2026-09-15 08:07:16 -0500

    temp

---
+++ software/practica_2/libs/personalizadas/impares.cpp
@@ -0,0 +1,22 @@
+﻿/*
+    @brief Halla la intersección entre los rectángulos A y B.
+
+    @param A Arreglo de 4 enteros que representa el primer rectángulo {x, y, ancho, alto}.
+    @param B Arreglo de 4 enteros que representa el segundo rectángulo {x, y, ancho, alto}.
+    @param C Arreglo de 4 enteros pasado por referencia (o puntero) donde se
+        almacenará el rectángulo resultante de la intersección.
+
+    @note Ejemplo de ejecución:
+    Si se ingresan los rectángulos A y B representados por los arreglos:
+    - A = {0, 0, 8, 4}
+    - B = {5, 2, 6, 7}
+    La función modificará el arreglo C para que contenga:
+    - C = {5, 0, 3, 4}
+*/
+void CalcularInterseccion(int A[], int B[], int C[]) {
+    int tempC[4];
+
+    // Verificar coordenadas x, y de c
+    // if()
+
+}
